9111b8deea39f431a2459a8e7c645fd3b72ea550
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2023-06-05 21:05:46 +0300

    Add notifications

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,11 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import uniqueId from "lodash/uniqueId";
+
+const PREFIX = "notification";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { isCartVisible: false },
+  initialState: {
+    isCartVisible: false,
+    notifications: [],
+  },
   reducers: {
     toggleCartVisiblity(state) {
       state.isCartVisible = !state.isCartVisible;
+    },
+    showErrorNotification(state, action) {
+      const { title, message } = action.payload;
+      const id = uniqueId(PREFIX);
+      state.notifications.push({ id, title, message, status: "error" });
+    },
+    showSuccessNotification(state, action) {
+      const { title, message } = action.payload;
+      const id = uniqueId(PREFIX);
+      state.notifications.push({ id, title, message, status: "success" });
+    },
+    hideNotification(state, action) {
+      state.notifications = state.notifications.filter(
+        (n) => n.id !== action.payload.id
+      );
     },
   },
 });
